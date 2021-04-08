d98781962a06bd98d6445721c7f2d1ae1cdac29f
AmadeoNeto <amadeotatocota@gmail.com>
2021-04-08 08:54:07 -0300

    Graphical interface using streamlit

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
+import streamlit as st
 from graph_builder import create_graph
 
 def print_header():
-    print("Analysis of Neural network of the nematode C. Elegans vertices distance")
-    print("Disponibilized by Mark Newman at: " +
-        "http://www-personal.umich.edu/~mejn/netdata/celegansneural.zip\n")
+    st.title("Analysis of Neural network of the nematode C. Elegans vertices distance")
+    st.write("Disponibilized by Mark Newman at:\n"+
+        "http://www-personal.umich.edu/~mejn/netdata/celegansneural.zip")
 
-    print("This algorithm will calculate the distance between 2 vertices in " +
-     "a directed graph that represents the neural network of C. Elegans.")
+    st.write("This algorithm will calculate the distance between 2 vertices in " +
+        "a directed graph that represents the neural network of C. Elegans.")
+    st.write("**Note:** there are 2 differents representation for each vertex" +
+        "by label(original) and by index(renumbered).")
 
-    print("Note: there are 2 differents representation for each vertex, " +
-     "by label(original) and by index(renumbered).\n")
+    st.image("https://royalsocietypublishing.org/cms/asset/07403866-d542-4089-a14c-ca5d72f8a973/rstb20140212f01.jpg")
+
 
 def search_by_label():
-    by_label = False
+    st.header("Search")
+    search_option = st.radio("Find path based on:", ("Indexes (recommended)","Labels"))
 
-    print("Do you wanna execute this algorithm based on labels or indexes?")
-    print("[1] By indexes (recommended)\n[2] By labels")
-    option = input()
-    if "1" in option:
+    if search_option == "Indexes (recommended)":
+        st.write("Vertex are in ther interval 0-296")
         by_label = False
-    elif "2" in option:
-        by_label = True
     else:
-        print("Invalid option")
-        quit()
+        st.write("Vertex are in ther interval 1-306 (some aren't in the database)")
+        by_label = True
 
     return by_label
 
@@ -31,42 +31,37 @@ def print_path(graph, start, target, by_label):
     dijkstra = graph.dijkstra(start, target, by_label)
 
     if dijkstra is None:
-        print(f"There is no path from {start} to {target}")
+        st.write(f"There is no path from {start} to {target}")
         return
 
     path = dijkstra[0]
     distance = dijkstra[1]
+    st.write(f"Distance: {distance}")
 
-    out = ""
-    out += f"Distance: {distance}\n"
-    out += "Shortest path: "
-
+    path_str = "Shortest path: "
     for i in range(len(path)-1):
-        out += f"{path[i]}, "
-    out += f"{path[-1]}\n"
-
-    print(out)
+        path_str += f"{path[i]}, "
+    path_str += f"{path[-1]}\n"
+    st.write(path_str)
 
 def main():
     print_header()
     by_label = search_by_label()
 
-    if by_label:
-        print("Vertex are in ther interval 1-306 (some aren't in the database)")
-    else:
-        print("Vertex are in ther interval 0-296")
-
     graph = create_graph()
-    print("Start Vertex: ", end="")
-    src = int(input())
-    print("Target Vertex: ", end="")
-    target = int(input())
-    print()
+    src = st.text_input("Start Vertex", help="The vertex where the search will start")
+    target = st.text_input("Target Vertex", help="The vertex where the search will end")
 
-    try:
-        print_path(graph, src, target, by_label)
-    except KeyError:
-        print("ERROR: One of the vertices is not availabe in the database!")
+    start_button = st.button("Find Path")
+    if start_button:
+        try:
+            src = int(src)
+            target = int(target)
+            print_path(graph, src, target, by_label)
+        except ValueError:
+            st.error("Value invalid or empty!")
+        except KeyError:
+            st.error("One of the vertices is not availabe in the database!")
 
 if __name__ == "__main__":
     main()
